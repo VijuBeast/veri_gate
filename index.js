fa25d10723add376213f5181dc5f940db8e6ba52
Vijay Singh Bisht <vsbrth.apache@gmail.com>
2026-09-18 16:05:00 +0530

    new changes from main

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
 // this is a console log
 console.log("hello world, from komal")
-<<<<<<< HEAD
 console.log("hello world from shyama and who is yash ?")
-=======
-console.log("hello world from shyama  is a good person")
->>>>>>> 8116a0217283dc6cea6165279b199d002d101be4
